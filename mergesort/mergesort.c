af483102a250243f6318381f176bd241b8c9f7d5
smil1201 <mill5978@morris.umn.edu>
2017-10-04 18:31:27 -0500

    Cleaned, corrected and commented code. Also added memory management.

--- mergesort/mergesort.c
+++ mergesort/mergesort.c
@@ -6,62 +6,80 @@
 
 //based on mergesort model from Nic Mcphee's Mergesort.java
 
+// function to check if our current section is large enough to still need sorting
 bool needsSorting(int rangeSize){
-	return rangeSize >= 2;
+	return (rangeSize >= 2);
 }
 
-void merge(int values[], int start, int mid, int end){
-	
-	//still needs memory management
+void merge(int values[], int start, int mid, int end){	
+	//initialize var and get value range
 	int rangeSize;
         rangeSize = end - start;
-	int target[rangeSize];
-	int firstInd;
-	firstInd = start;
-	int secondInd;
-	secondInd = mid;
-	int copyInd = 0;
+	
+	//make array and allocate space for it
+	int* target;
+	target = (int*) calloc(rangeSize+1,sizeof(int));
+	
+	//initialize index vars
+	int first, second;
+	first = start;
+	second = mid;
+	int copy = 0;
 
-	while(firstInd < mid && secondInd < end) {
-		if(values[firstInd] < values[secondInd]){
-			target[copyInd] = values[firstInd];
-			++firstInd;
+	//iterate through array, comparing vars and setting destination array
+	while(first < mid && second < end) {
+		if(values[first] < values[second]){
+			target[copy] = values[first];
+			++first;
 		} else {
-			target[copyInd] = values[secondInd];
-			++secondInd;
+			target[copy] = values[second];
+			++second;
 		}
-		
-		++copyInd;
+		++copy;
 	}
 
-	while(firstInd < mid){
-		target[copyInd] = values[firstInd];
-		++copyInd;
-		++firstInd;
+	while(first < mid){
+		target[copy] = values[first];
+		++copy;
+		++first;
 	}
 	
-	while(secondInd < end){
-		target[copyInd] = values[secondInd];
-		++copyInd;
-		++secondInd;
+	while(second < end){
+		target[copy] = values[second];
+		++copy;
+		++second;
 	}
-
+	
+	//add null terminator
+	target[rangeSize]='\0';
+	
+	//set array to correct values
 	int i;
-
 	for(i = 0; i < rangeSize; ++i){
 		values[i + start] = target[i];
 	}
+	
+	//free allocated memory
+	free(target);
 }
 		
-void mergesort(int values[], int start, int end){
-        //still needs memory management
+void sort(int values[], int start, int end){
+	//sets range, and if large enough it splits and sorts elements recursively
 	int rangeSize;
         rangeSize = end - start;
 	if(needsSorting(rangeSize)){
 		int mid;
 	        mid = (start + end) / 2;
-		mergesort(values, start, mid);
-		mergesort(values, mid, end);
+		sort(values, start, mid);
+		sort(values, mid, end);
 		merge(values, start, mid, end);	
-	}
+	} 
+}
+
+void mergesort(int size, int values[]){
+	//determine index vars, and calls sort
+	int start = 0;
+	int end;
+	end = size;
+	sort(values, start, end);
 }
