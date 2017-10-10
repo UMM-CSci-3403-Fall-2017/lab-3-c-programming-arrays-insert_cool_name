53080e3804e885d4e9b037c6073320f3b583091a
smil1201 <mill5978@morris.umn.edu>
2017-10-09 21:38:26 -0500

    Restructured into subfunctions, fixed bugs and commented code.

--- array_merge/array_merge.c
+++ array_merge/array_merge.c
@@ -4,53 +4,85 @@
 #include "array_merge.h"
 #include "../mergesort/mergesort.h"
 
+int get_count(int size, int* arr){
+	//initialize vars	
+	int holder = '\0';
+        int count = 0;
+        bool zeroesCounted = false;
+	
+	//iterate through, incrementing count for each unique value
+	for(int i = 0; i < size; i++){
+		//handle case of values matching our initial holder value
+		if((arr[i] == 0) && (zeroesCounted == false) && (i != size-1)){
+			count++;
+			zeroesCounted=true;
+		} if(arr[i] != holder){
+                        count++;
+                        holder = arr[i];
+                }
+        }
+
+	return count;
+}
+
+void set_array(int uniqCount, int size, int* input, int* output) {
+	//initialize vars and set first element to # of unique values	
+	int holder = '\0';
+        output[0] = uniqCount;
+        int n = 1;
+	bool zeroesCounted = false;
+
+	//iterate through, setting output to unique values from input
+        for(int v = 0; v < size; v++){
+		//handle zero case
+		if((input[v] == 0) && (zeroesCounted == false) && (v != size-1)){
+			output[n] = input[v];
+			n++;
+			zeroesCounted=true;
+		}
+	       	if(input[v] != holder){
+                        output[n] = input[v];
+                        n++;
+                        holder = input[v];
+                }
+        }
+}	
+
+
 int* array_merge(int num_arrays, int* sizes, int** values){
-	int thisSize=0;
-	int counter = 0;
+	//initialize vars
+	int arrCount=0;
         int* temp;
 
-	temp = (int*) calloc(thisSize + 1, sizeof(int));
+        //get number of values and make an array of that size
 	for(int i = 0; i < num_arrays; i++){
-                for(int q=0; q<sizes[i]; q++){
-        	       	temp[counter] = values[i][q];
-			thisSize++;
-			counter++;
-                }
-        }
-        
-        mergesort(thisSize, temp);
-        
-	int holder = '\0';
-        int theCount = 0;
-        	
-	for(int i = 0; i < thisSize; i++){
-                if(temp[i] != holder){
-                        theCount++;
-                        holder = temp[i];
-                }
+                arrCount+=sizes[i];
         }
 
+        temp = (int*) calloc(arrCount + 1, sizeof(int));
+  
+	//iterate through and set this array to input values
+	int counter = 0;
+        for(int j = 0; j < num_arrays; j++){
+                for(int q = 0; q < sizes[j]; q++){
+                	temp[counter] = values[j][q];
+                	counter++;
+                }
+        }
+	
+	//sort
+        mergesort(arrCount, temp);
+	
+	//find amount of unique elements
+	int count;
+	count = get_count(arrCount, temp);
+	
+	//generate output array and populate it with count and unique values
         int* output;
-        output = (int*) calloc(theCount + 1, sizeof(int));
-        
-	int secondHolder = '\0';
-        output[0] = theCount;
-        int n = 1;
-        for(int v = 0; v < thisSize; v++){
-                if(temp[v] != secondHolder){
-                        output[n] = temp[v];
-                        n++;
-                        secondHolder = temp[v];
-                }
-
-        }
-
-	/*
-        for(int n = 0; n <= theCount; n++){
-                printf("element [%i] is %i\n",n,output[n]);
-        }
-	*/
-
+        output = (int*) calloc(count + 1, sizeof(int));
+	set_array(count, arrCount, temp, output);
+	
+	//free used memory and return result
 	free(temp);
 	return(output);
 }
